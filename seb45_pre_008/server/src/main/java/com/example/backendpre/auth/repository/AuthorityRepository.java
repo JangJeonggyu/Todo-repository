8674fd62617dc8867768f53c4acfdaa33e9fb860
JangJeonggyu <129978430+JangJeonggyu@users.noreply.github.com>
2023-08-18 13:50:41 +0900

    Delete seb45_pre_008 directory

--- seb45_pre_008/server/src/main/java/com/example/backendpre/auth/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backendpre.auth.repository;
-
-import com.example.backendpre.auth.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
